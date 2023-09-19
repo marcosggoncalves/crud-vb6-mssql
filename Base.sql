5e3874c238733e9b7005acbe63b113fdad89b255
Marcos Gonçalves <marcoslopesg7@gmail.com>
2023-09-19 09:39:58 -0300

    Update Base.sql

--- Base.sql
+++ Base.sql
@@ -116,3 +116,29 @@ GO
 ALTER DATABASE [gestfro] SET  READ_WRITE 
 GO
 
+
+USE [gestfro]
+GO
+
+/****** Object:  Table [dbo].[veiculo]    Script Date: 19/09/2023 09:39:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[veiculo](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[placa] [varchar](255) NULL,
+	[descricao] [varchar](255) NULL,
+	[marca] [varchar](255) NULL,
+	[modelo] [varchar](255) NULL,
+	[quantidade_lugares] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
